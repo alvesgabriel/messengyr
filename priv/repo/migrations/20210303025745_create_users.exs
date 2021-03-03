f5b0482d33f8fe5b49d68449ee48361379d930ba
Gabriel Alves <gabriel.alves.monteiro1@gmail.com>
2021-03-03 20:25:13 -0300

    :sparkles: Created user schema
    close #3

---
+++ priv/repo/migrations/20210303025745_create_users.exs
@@ -0,0 +1,16 @@
+defmodule Messengyr.Repo.Migrations.CreateUsers do
+  use Ecto.Migration
+
+  def change do
+    create table(:users) do
+      add :username, :string, unique: true
+      add :email, :string, unique: true
+      add :encrypted_password, :string
+
+      timestamps()
+    end
+
+    create unique_index(:users, [:username])
+    create unique_index(:users, [:email])
+  end
+end
