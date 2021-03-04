158774c1e7efeb1d5748635e13e0fc6ed40a6d46
Gabriel Alves <gabriel.alves.monteiro1@gmail.com>
2021-03-03 23:32:06 -0300

    :sparkles: Configured sign up form page
    close #5

--- lib/messengyr_web/controllers/page_controller.ex
+++ lib/messengyr_web/controllers/page_controller.ex
@@ -1,12 +1,29 @@
 defmodule MessengyrWeb.PageController do
   use MessengyrWeb, :controller
+  alias Messengyr.Accounts
 
   def index(conn, _params) do
     render(conn)
   end
 
   def signup(conn, _params) do
-    render(conn)
+    changeset = Accounts.register_changeset()
+
+    render(conn, user_changeset: changeset)
+  end
+
+  def create_user(conn, %{"user" => user_params}) do
+    case Accounts.create_user(user_params) do
+      {:ok, _user} ->
+        conn
+        |> put_flash(:info, "User created successfuly!")
+        |> redirect(to: "/")
+
+      {:error, user_changeset} ->
+        conn
+        |> put_flash(:error, "Unable to create account!")
+        |> render("signup.html", user_changeset: user_changeset)
+    end
   end
 
   def login(conn, _params) do
